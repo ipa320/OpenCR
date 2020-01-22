ec553d6c6f27be222ac630ee0900328649194a69
Jannik Abbenseth <jannik.abbenseth@ipa.fraunhofer.de>
2020-01-22 17:05:48 +0100

    Strip mecanum core to joint state interfaces

--- arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_friends/turtlebot3_mecanum_core/turtlebot3_mecanum_core_config.h
+++ arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_friends/turtlebot3_mecanum_core/turtlebot3_mecanum_core_config.h
@@ -1,18 +1,18 @@
 /*******************************************************************************
-* Copyright 2016 ROBOTIS CO., LTD.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright 2016 ROBOTIS CO., LTD.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 
 /* Authors: Yoonseok Pyo, Leon Jung, Darby Lim, HanCheol Cho, Valentin Starlinger */
 
@@ -23,6 +23,7 @@
 #include <ros/time.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/Empty.h>
+#include <std_msgs/Byte.h>
 #include <std_msgs/Int32.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/JointState.h>
@@ -50,181 +51,132 @@
 #define SOFTWARE_VER "1.0.0"
 #define FIRMWARE_VER "1.0.15"
 
-#define CONTROL_MOTOR_SPEED_PERIOD          30   //hz
-#define IMU_PUBLISH_PERIOD                  100  //hz
-#define CMD_VEL_PUBLISH_PERIOD              30   //hz
-#define DRIVE_INFORMATION_PUBLISH_PERIOD    30   //hz
-#define VERSION_INFORMATION_PUBLISH_PERIOD  1    //hz 
+#define CONTROL_MOTOR_SPEED_PERIOD 30         // hz
+#define CONTROL_MOTOR_TIMEOUT 100             // ms
+#define IMU_PUBLISH_PERIOD 100                // hz
+#define CMD_VEL_PUBLISH_PERIOD 30             // hz
+#define DRIVE_INFORMATION_PUBLISH_PERIOD 30   // hz
+#define VERSION_INFORMATION_PUBLISH_PERIOD 1  // hz
 
-#define NUM_WHEELS                       4
-#define ENCODER_MIN                      -2147483648     // raw
-#define ENCODER_MAX                      2147483648      // raw
+#define NUM_WHEELS 4
+#define ENCODER_MIN -2147483648  // raw
+#define ENCODER_MAX 2147483648   // raw
 
-#define WHEEL_RADIUS                    0.1      // meter
-#define WHEEL_SEPARATION_X              0.09    // meter
-#define WHEEL_SEPARATION_Y              0.115     // meter
-#define DISTANCE_CENTER_TO_WHEEL        0.165     // meter
+#define MAX_WHEEL_VEL 300  // 0.229 rev/60s
 
-#define LEFT_FRONT                            0
-#define RIGHT_FRONT                           1
-#define LEFT_REAR                             2
-#define RIGHT_REAR                            3
-
-#define LINEAR_X                              0
-#define LINEAR_Y                              1
-#define ANGULAR                               2
-
-#define MAX_LINEAR_VELOCITY              0.25   // m/s   (BURGER : 0.22, WAFFLE : 0.25)
-#define MAX_ANGULAR_VELOCITY             1.82   // rad/s (BURGER : 2.84, WAFFLE : 1.82)
-
-#define TICK2RAD                         0.001533981  // 0.087890625[deg] * 3.14159265359 / 180 = 0.001533981f
-
-#define DEG2RAD(x)                       (x * 0.01745329252)  // *PI/180
-#define RAD2DEG(x)                       (x * 57.2957795131)  // *180/PI
+#define TICK2RAD 0.001533981            // 0.087890625[deg] * 3.14159265359 / 180 = 0.001533981f
+#define RAD2VEL 41.6999850896           //
+#define DEG2RAD(x) (x * 0.01745329252)  // *PI/180
+#define RAD2DEG(x) (x * 57.2957795131)  // *180/PI
 
 // Callback function prototypes
-void commandVelocityCallback(const geometry_msgs::Twist& cmd_vel_msg);
-void soundCallback(const turtlebot3_msgs::Sound& sound_msg);
+void commandJointStateCallback(const sensor_msgs::JointState& cmd_msg);
 void motorPowerCallback(const std_msgs::Bool& power_msg);
 void resetCallback(const std_msgs::Empty& reset_msg);
 
 // Function prototypes
-void publishImuMsg(void);
-void publishMagMsg(void);
-void publishSensorStateMsg(void);
-void publishVersionInfoMsg(void);
-void publishBatteryStateMsg(void);
 void publishDriveInformation(void);
 
 ros::Time rosNow(void);
-ros::Time addMicros(ros::Time & t, uint32_t _micros);
+ros::Time addMicros(ros::Time& t, uint32_t _micros);
 
-void updateVariable(void);
-void updateMotorInfo(int32_t left_front_tick, int32_t right_front_tick, int32_t left_rear_tick, int32_t right_rear_tick);
+void updateMotorInfo();
 void updateTime(void);
-void updateOdometry(void);
-void updateJoint(void);
-void updateTF(geometry_msgs::TransformStamped& odom_tf);
-void updateGyroCali(void);
-void updateVoltageCheck(void);
 
-void initOdom(void);
 void initJointStates(void);
 
-bool calcOdometry(double diff_time);
-
 void sendLogMsg(void);
-void melody(uint16_t* note, uint8_t note_num, uint8_t* durations);
+
+/*****
+ * Kinetmatik stuff
+ * Motor ids: front left, front right, rear left, rear right
+ **** */
+
+int32_t motor_ids_[NUM_WHEELS] = { 1, 2, 3, 4 };
+char* joint_states_name[NUM_WHEELS] = {
+  "wheel_front_left_joint",
+  "wheel_rear_left_joint",
+  "wheel_rear_right_joint",
+  "wheel_front_right_joint",
+};
 
 /*******************************************************************************
-* ROS NodeHandle
-*******************************************************************************/
+ * ROS NodeHandle
+ *******************************************************************************/
 ros::NodeHandle nh;
 ros::Time current_time;
 uint32_t current_offset;
 
 /*******************************************************************************
-* Subscriber
-*******************************************************************************/
-ros::Subscriber<geometry_msgs::Twist> cmd_vel_sub("cmd_vel", commandVelocityCallback);
-
-ros::Subscriber<turtlebot3_msgs::Sound> sound_sub("sound", soundCallback);
+ * Subscriber
+ *******************************************************************************/
+ros::Subscriber<sensor_msgs::JointState> cmd_js_sub_("joint_command", commandJointStateCallback);
 
 ros::Subscriber<std_msgs::Bool> motor_power_sub("motor_power", motorPowerCallback);
 
 ros::Subscriber<std_msgs::Empty> reset_sub("reset", resetCallback);
 
 /*******************************************************************************
-* Publisher
-*******************************************************************************/
-// Bumpers, cliffs, buttons, encoders, battery of Turtlebot3
-turtlebot3_msgs::SensorState sensor_state_msg;
-ros::Publisher sensor_state_pub("sensor_state", &sensor_state_msg);
+ * Publisher
+ *******************************************************************************/
 
-// Version information of Turtlebot3
-turtlebot3_msgs::VersionInfo version_info_msg;
-ros::Publisher version_info_pub("version_info", &version_info_msg);
-
-// IMU of Turtlebot3
-sensor_msgs::Imu imu_msg;
-ros::Publisher imu_pub("imu", &imu_msg);
-
-// Command velocity of Turtlebot3 using RC100 remote controller
-geometry_msgs::Twist cmd_vel_rc100_msg;
-ros::Publisher cmd_vel_rc100_pub("cmd_vel_rc100", &cmd_vel_rc100_msg);
-
-// Odometry of Turtlebot3
-nav_msgs::Odometry odom;
-ros::Publisher odom_pub("odom_combined", &odom);
+std_msgs::Byte status_byte_msg_;
+ros::Publisher status_byte_pub_("status_byte", &status_byte_msg_);
 
 // Joint(Dynamixel) state of Turtlebot3
 sensor_msgs::JointState joint_states;
 ros::Publisher joint_states_pub("joint_states", &joint_states);
 
-// Battey state of Turtlebot3
-sensor_msgs::BatteryState battery_state_msg;
-ros::Publisher battery_state_pub("battery_state", &battery_state_msg);
-
-// Magnetic field
-sensor_msgs::MagneticField mag_msg;
-ros::Publisher mag_pub("magnetic_field", &mag_msg);
+/*******************************************************************************
+ * SoftwareTimer of Turtlebot3
+ *******************************************************************************/
+static uint32_t tTime[6];
 
 /*******************************************************************************
-* Transform Broadcaster
-*******************************************************************************/
-// TF of Turtlebot3
-geometry_msgs::TransformStamped odom_tf;
-tf::TransformBroadcaster tf_broadcaster;
+ * Declaration for motor
+ *******************************************************************************/
+Turtlebot3MecanumCoreMotorDriver motor_driver(motor_ids_[0], motor_ids_[1], motor_ids_[2], motor_ids_[3]);
 
 /*******************************************************************************
-* SoftwareTimer of Turtlebot3
-*******************************************************************************/
-static uint32_t tTime[5];
+ * Calculation for odometry
+ *******************************************************************************/
+bool init_encoder;
+int32_t last_tick[NUM_WHEELS] = { 0.0, 0.0, 0.0, 0.0 };
+int32_t last_diff_tick[NUM_WHEELS] = { 0.0, 0.0, 0.0, 0.0 };
+double last_rad[NUM_WHEELS] = { 0.0, 0.0, 0.0, 0.0 };
+
+double target_velocity[NUM_WHEELS] = { 0.0, 0.0, 0.0, 0.0 };
 
 /*******************************************************************************
-* Declaration for motor
-*******************************************************************************/
-Turtlebot3MecanumCoreMotorDriver motor_driver;
+ * Update Joint State
+ *******************************************************************************/
+double last_velocity[NUM_WHEELS] = { 0.0, 0.0, 0.0, 0.0 };
 
 /*******************************************************************************
-* Calculation for odometry
-*******************************************************************************/
-bool init_encoder = true;
-int32_t last_diff_tick[NUM_WHEELS] = {0.0, 0.0, 0.0, 0.0};
-double  last_rad[NUM_WHEELS]       = {0.0, 0.0, 0.0, 0.0};
+ * Declaration for sensors
+ *******************************************************************************/
+// Turtlebot3Sensor sensors;
 
 /*******************************************************************************
-* Update Joint State
-*******************************************************************************/
-double  last_velocity[NUM_WHEELS]  = {0.0, 0.0, 0.0, 0.0};
+ * Declaration for controllers
+ *******************************************************************************/
+// Turtlebot3Controller controllers;
+float goal_velocity[3] = { 0.0, 0.0, 0.0 };
 
 /*******************************************************************************
-* Declaration for sensors
-*******************************************************************************/
-Turtlebot3Sensor sensors;
+ * Declaration for diagnosis
+ *******************************************************************************/
+// Turtlebot3Diagnosis diagnosis;
 
 /*******************************************************************************
-* Declaration for controllers
-*******************************************************************************/
-Turtlebot3Controller controllers;
-float goal_velocity[3] = {0.0, 0.0, 0.0};
-
-/*******************************************************************************
-* Declaration for diagnosis
-*******************************************************************************/
-Turtlebot3Diagnosis diagnosis;
-
-/*******************************************************************************
-* Declaration for SLAM and navigation
-*******************************************************************************/
+ * Declaration for SLAM and navigation
+ *******************************************************************************/
 unsigned long prev_update_time;
-float odom_pose[3];
-double odom_vel[3];
 
 /*******************************************************************************
-* Declaration for Battery
-*******************************************************************************/
-bool setup_end        = false;
+ * Declaration for Battery
+ *******************************************************************************/
+bool setup_end = false;
 uint8_t battery_state = 0;
 
-#endif // TURTLEBOT3_MECANUM_CORE_CONFIG_H_
+#endif  // TURTLEBOT3_MECANUM_CORE_CONFIG_H_
--- arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_friends/turtlebot3_mecanum_core/turtlebot3_mecanum_core_motor_driver.cpp
+++ arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_friends/turtlebot3_mecanum_core/turtlebot3_mecanum_core_motor_driver.cpp
@@ -1,31 +1,45 @@
 /*******************************************************************************
-* Copyright 2016 ROBOTIS CO., LTD.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+ * Copyright 2016 ROBOTIS CO., LTD.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 
 /* Authors: Yoonseok Pyo, Leon Jung, Darby Lim, HanCheol Cho, Valentin Starlinger */
 
 #include "turtlebot3_mecanum_core_motor_driver.h"
 
 Turtlebot3MecanumCoreMotorDriver::Turtlebot3MecanumCoreMotorDriver()
-: baudrate_(BAUDRATE),
-  protocol_version_(PROTOCOL_VERSION),
-  left_front_wheel_id_(DXL_LEFT_FRONT_ID),
-  right_front_wheel_id_(DXL_RIGHT_FRONT_ID),
-  left_rear_wheel_id_(DXL_LEFT_REAR_ID),
-  right_rear_wheel_id_(DXL_RIGHT_REAR_ID),
-  torque_(false)
+  : baudrate_(BAUDRATE)
+  , protocol_version_(PROTOCOL_VERSION)
+  , left_front_wheel_id_(DXL_LEFT_FRONT_ID)
+  , right_front_wheel_id_(DXL_RIGHT_FRONT_ID)
+  , left_rear_wheel_id_(DXL_LEFT_REAR_ID)
+  , right_rear_wheel_id_(DXL_RIGHT_REAR_ID)
+  , torque_(false)
+{
+}
+
+Turtlebot3MecanumCoreMotorDriver::Turtlebot3MecanumCoreMotorDriver(uint8_t left_front_motor_id,
+                                                                   uint8_t right_front_motor_id,
+                                                                   uint8_t left_rear_motor_id,
+                                                                   uint8_t right_rear_motor_id)
+  : baudrate_(BAUDRATE)
+  , protocol_version_(PROTOCOL_VERSION)
+  , left_front_wheel_id_(left_front_motor_id)
+  , right_front_wheel_id_(right_front_motor_id)
+  , left_rear_wheel_id_(left_rear_motor_id)
+  , right_rear_wheel_id_(right_rear_motor_id)
+  , torque_(false)
 {
 }
 
@@ -36,7 +50,7 @@ Turtlebot3MecanumCoreMotorDriver::~Turtlebot3MecanumCoreMotorDriver()
 
 bool Turtlebot3MecanumCoreMotorDriver::init(void)
 {
-  portHandler_   = dynamixel::PortHandler::getPortHandler(DEVICENAME);
+  portHandler_ = dynamixel::PortHandler::getPortHandler(DEVICENAME);
   packetHandler_ = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
 
   // Open port
@@ -57,8 +71,10 @@ bool Turtlebot3MecanumCoreMotorDriver::init(void)
   setTorque(left_front_wheel_id_, true);
   setTorque(right_front_wheel_id_, true);
 
-  groupSyncWriteVelocity_ = new dynamixel::GroupSyncWrite(portHandler_, packetHandler_, ADDR_X_GOAL_VELOCITY, LEN_X_GOAL_VELOCITY);
-  groupSyncReadEncoder_   = new dynamixel::GroupSyncRead(portHandler_, packetHandler_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
+  groupSyncWriteVelocity_ =
+      new dynamixel::GroupSyncWrite(portHandler_, packetHandler_, ADDR_X_GOAL_VELOCITY, LEN_X_GOAL_VELOCITY);
+  groupSyncReadEncoder_ =
+      new dynamixel::GroupSyncRead(portHandler_, packetHandler_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
 
   return true;
 }
@@ -69,12 +85,12 @@ bool Turtlebot3MecanumCoreMotorDriver::setTorque(uint8_t id, bool onoff)
   int dxl_comm_result = COMM_TX_FAIL;
 
   dxl_comm_result = packetHandler_->write1ByteTxRx(portHandler_, id, ADDR_X_TORQUE_ENABLE, onoff, &dxl_error);
-  if(dxl_comm_result != COMM_SUCCESS)
+  if (dxl_comm_result != COMM_SUCCESS)
   {
     Serial.println(packetHandler_->getTxRxResult(dxl_comm_result));
     return false;
   }
-  else if(dxl_error != 0)
+  else if (dxl_error != 0)
   {
     Serial.println(packetHandler_->getRxPacketError(dxl_error));
     return false;
@@ -101,12 +117,16 @@ void Turtlebot3MecanumCoreMotorDriver::closeDynamixel(void)
   portHandler_->closePort();
 }
 
-bool Turtlebot3MecanumCoreMotorDriver::readEncoder(int32_t &left_front_value, int32_t  &right_front_value, int32_t  &left_rear_value, int32_t  &right_rear_value) {
-  int dxl_comm_result = COMM_TX_FAIL;              // Communication result
-  bool dxl_addparam_result = false;                // addParam result
-  bool dxl_getdata_result = false;                 // GetParam result
+bool Turtlebot3MecanumCoreMotorDriver::readEncoder(int32_t& left_front_value,
+                                                   int32_t& right_front_value,
+                                                   int32_t& left_rear_value,
+                                                   int32_t& right_rear_value)
+{
+  int dxl_comm_result = COMM_TX_FAIL;  // Communication result
+  bool dxl_addparam_result = false;    // addParam result
+  bool dxl_getdata_result = false;     // GetParam result
 
-   // Set parameter
+  // Set parameter
   dxl_addparam_result = groupSyncReadEncoder_->addParam(left_rear_wheel_id_);
   if (dxl_addparam_result != true)
     return false;
@@ -128,40 +148,53 @@ bool Turtlebot3MecanumCoreMotorDriver::readEncoder(int32_t &left_front_value, in
   if (dxl_comm_result != COMM_SUCCESS)
     Serial.println(packetHandler_->getTxRxResult(dxl_comm_result));
 
-   // Check if groupSyncRead data of Dynamixels are available
-  dxl_getdata_result = groupSyncReadEncoder_->isAvailable(left_rear_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
+  // Check if groupSyncRead data of Dynamixels are available
+  dxl_getdata_result =
+      groupSyncReadEncoder_->isAvailable(left_rear_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
   if (dxl_getdata_result != true)
     return false;
 
-  dxl_getdata_result = groupSyncReadEncoder_->isAvailable(right_rear_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
-  if (dxl_getdata_result != true) 
-    return false;
-
-  dxl_getdata_result = groupSyncReadEncoder_->isAvailable(left_front_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
+  dxl_getdata_result =
+      groupSyncReadEncoder_->isAvailable(right_rear_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
   if (dxl_getdata_result != true)
     return false;
 
-  dxl_getdata_result = groupSyncReadEncoder_->isAvailable(right_front_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
+  dxl_getdata_result =
+      groupSyncReadEncoder_->isAvailable(left_front_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
+  if (dxl_getdata_result != true)
+    return false;
+
+  dxl_getdata_result =
+      groupSyncReadEncoder_->isAvailable(right_front_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
   if (dxl_getdata_result != true)
     return false;
 
   // Get data
-  left_rear_value  = groupSyncReadEncoder_->getData(left_rear_wheel_id_,  ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
-  right_rear_value = groupSyncReadEncoder_->getData(right_rear_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
-  left_front_value  = groupSyncReadEncoder_->getData(left_front_wheel_id_,  ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
-  right_front_value = groupSyncReadEncoder_->getData(right_front_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
+  left_rear_value =
+      groupSyncReadEncoder_->getData(left_rear_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
+  right_rear_value =
+      groupSyncReadEncoder_->getData(right_rear_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
+  left_front_value =
+      groupSyncReadEncoder_->getData(left_front_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
+  right_front_value =
+      groupSyncReadEncoder_->getData(right_front_wheel_id_, ADDR_X_PRESENT_POSITION, LEN_X_PRESENT_POSITION);
 
   groupSyncReadEncoder_->clearParam();
   return true;
 }
 
-bool Turtlebot3MecanumCoreMotorDriver::writeVelocity(int64_t left_front_value, int64_t right_front_value, int64_t left_rear_value, int64_t right_rear_value)
+bool Turtlebot3MecanumCoreMotorDriver::writeVelocity(int64_t left_front_value,
+                                                     int64_t right_front_value,
+                                                     int64_t left_rear_value,
+                                                     int64_t right_rear_value)
 {
   bool dxl_addparam_result;
   int8_t dxl_comm_result;
 
-  int64_t value[4] = {left_front_value, right_front_value,left_rear_value, right_rear_value};
-  uint8_t data_byte[4] = {0, };
+  int64_t value[4] = { left_front_value, right_front_value, left_rear_value, right_rear_value };
+  uint8_t data_byte[4] = {
+    0,
+  };
 
   for (uint8_t index = 0; index < 4; index++)
   {
@@ -170,7 +203,7 @@ bool Turtlebot3MecanumCoreMotorDriver::writeVelocity(int64_t left_front_value, i
     data_byte[2] = DXL_LOBYTE(DXL_HIWORD(value[index]));
     data_byte[3] = DXL_HIBYTE(DXL_HIWORD(value[index]));
 
-    dxl_addparam_result = groupSyncWriteVelocity_->addParam(index+1, (uint8_t*)&data_byte);
+    dxl_addparam_result = groupSyncWriteVelocity_->addParam(index + 1, (uint8_t*)&data_byte);
     if (dxl_addparam_result != true)
       return false;
   }
@@ -186,37 +219,48 @@ bool Turtlebot3MecanumCoreMotorDriver::writeVelocity(int64_t left_front_value, i
   return true;
 }
 
-bool Turtlebot3MecanumCoreMotorDriver::controlMotor(const float r, const float wheel_separation_x, const float wheel_separation_y, float* value)
+bool Turtlebot3MecanumCoreMotorDriver::controlMotor(const float r,
+                                                    const float wheel_separation_x,
+                                                    const float wheel_separation_y,
+                                                    float* value)
 {
   bool dxl_comm_result = false;
 
   float wheel_velocity_rad_per_s[4];
-  float goal_velocity_0_229_rpm[4]; //unit for dynamixels is 0.229 rpm
+  float goal_velocity_0_229_rpm[4];  // unit for dynamixels is 0.229 rpm
 
   float lin_x_vel = value[0];
   float lin_y_vel = value[1];
-  float ang_vel   = value[2];
+  float ang_vel = value[2];
 
-  wheel_velocity_rad_per_s[LEFT_FRONT]  = (1/r) * (lin_x_vel - lin_y_vel - (wheel_separation_x + wheel_separation_y) * ang_vel);
-  wheel_velocity_rad_per_s[RIGHT_FRONT] = (-1) * (1/r) * (lin_x_vel + lin_y_vel + (wheel_separation_x + wheel_separation_y) * ang_vel);
-  wheel_velocity_rad_per_s[LEFT_REAR]   = (1/r) * (lin_x_vel + lin_y_vel - (wheel_separation_x + wheel_separation_y) * ang_vel);
-  wheel_velocity_rad_per_s[RIGHT_REAR]  = (-1) * (1/r) * (lin_x_vel - lin_y_vel + (wheel_separation_x + wheel_separation_y) * ang_vel);
+  wheel_velocity_rad_per_s[LEFT_FRONT] =
+      (1 / r) * (lin_x_vel - lin_y_vel - (wheel_separation_x + wheel_separation_y) * ang_vel);
+  wheel_velocity_rad_per_s[RIGHT_FRONT] =
+      (-1) * (1 / r) * (lin_x_vel + lin_y_vel + (wheel_separation_x + wheel_separation_y) * ang_vel);
+  wheel_velocity_rad_per_s[LEFT_REAR] =
+      (1 / r) * (lin_x_vel + lin_y_vel - (wheel_separation_x + wheel_separation_y) * ang_vel);
+  wheel_velocity_rad_per_s[RIGHT_REAR] =
+      (-1) * (1 / r) * (lin_x_vel - lin_y_vel + (wheel_separation_x + wheel_separation_y) * ang_vel);
 
-  for (int id = 0; id < 4; id++) {
-    //calculate 0.229 rmp from rad/s
+  for (int id = 0; id < 4; id++)
+  {
+    // calculate 0.229 rmp from rad/s
     goal_velocity_0_229_rpm[id] = wheel_velocity_rad_per_s[id] * 60 / (2 * PI * r * 0.229);
 
-    if (goal_velocity_0_229_rpm[id] > LIMIT_X_MAX_VELOCITY){
-	    goal_velocity_0_229_rpm[id] =  LIMIT_X_MAX_VELOCITY;
-    } else if (goal_velocity_0_229_rpm[id] < -LIMIT_X_MAX_VELOCITY){
-	    goal_velocity_0_229_rpm[id] = -LIMIT_X_MAX_VELOCITY;
-    } 
-  }  
- 
+    if (goal_velocity_0_229_rpm[id] > LIMIT_X_MAX_VELOCITY)
+    {
+      goal_velocity_0_229_rpm[id] = LIMIT_X_MAX_VELOCITY;
+    }
+    else if (goal_velocity_0_229_rpm[id] < -LIMIT_X_MAX_VELOCITY)
+    {
+      goal_velocity_0_229_rpm[id] = -LIMIT_X_MAX_VELOCITY;
+    }
+  }
+
   dxl_comm_result = writeVelocity((int64_t)goal_velocity_0_229_rpm[LEFT_FRONT],
-				  (int64_t)goal_velocity_0_229_rpm[RIGHT_FRONT], 
-				  (int64_t)goal_velocity_0_229_rpm[LEFT_REAR], 
-				  (int64_t)goal_velocity_0_229_rpm[RIGHT_REAR]);
+                                  (int64_t)goal_velocity_0_229_rpm[RIGHT_FRONT],
+                                  (int64_t)goal_velocity_0_229_rpm[LEFT_REAR],
+                                  (int64_t)goal_velocity_0_229_rpm[RIGHT_REAR]);
   if (dxl_comm_result == false)
     return false;
 
